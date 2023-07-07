f3dcde792597fcc73a24a32c2caa66e249e86554
lizonghao <zonghao.li@mihoyo.com>
2023-07-07 10:13:00 +0800

    feat: init

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -8,22 +8,80 @@ export default defineConfig({
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Docs', link: '/docs/' }
-      // { text: 'Examples', link: '/markdown-examples' }
+      { text: 'JavaScript', link: '/docs/JavaScript/可取消的Promise' },
+      { text: 'Examples', link: '/markdown-examples' },
     ],
 
-    // sidebar: [
-    //   {
-    //     text: 'Examples',
-    //     items: [
-    //       { text: 'Markdown Examples', link: '/markdown-examples' },
-    //       { text: 'Runtime API Examples', link: '/api-examples' }
-    //     ]
-    //   }
-    // ],
+    sidebar: [
+      {
+        text: 'JavaScript',
+        items: [
+          { text: '可取消的Promise', link: '/docs/JavaScript/可取消的Promise' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      },
+      {
+        text: 'Examples',
+        items: [
+          { text: '可取消的Promise', link: '/docs/JavaScript/可取消的Promise' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
 
     // socialLinks: [
     //   { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
     // ]
+    search: {
+      provider: 'algolia',
+      options: {
+        appId: 'OFN60WLTSV',
+        apiKey: 'aae3e87abc84ce87b3df8ce8d30e80cd',
+        indexName: 'notes',
+        locales: {
+          zh: {
+            placeholder: '搜索文档',
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档'
+              },
+              modal: {
+                searchBox: {
+                  resetButtonTitle: '清除查询条件',
+                  resetButtonAriaLabel: '清除查询条件',
+                  cancelButtonText: '取消',
+                  cancelButtonAriaLabel: '取消'
+                },
+                startScreen: {
+                  recentSearchesTitle: '搜索历史',
+                  noRecentSearchesText: '没有搜索历史',
+                  saveRecentSearchButtonTitle: '保存至搜索历史',
+                  removeRecentSearchButtonTitle: '从搜索历史中移除',
+                  favoriteSearchesTitle: '收藏',
+                  removeFavoriteSearchButtonTitle: '从收藏中移除'
+                },
+                errorScreen: {
+                  titleText: '无法获取结果',
+                  helpText: '你可能需要检查你的网络连接'
+                },
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                  closeText: '关闭',
+                  searchByText: '搜索提供者'
+                },
+                noResultsScreen: {
+                  noResultsText: '无法找到相关结果',
+                  suggestedQueryText: '你可以尝试查询',
+                  reportMissingResultsText: '你认为该查询应该有结果？',
+                  reportMissingResultsLinkText: '点击反馈'
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 })
